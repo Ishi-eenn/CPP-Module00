1430a03d10ccf3b0115ac6f509f769050ce3e3dd
Ishi <syi378039@gmail.com>
2023-12-24 14:47:53 +0900

    add : ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 00:59:12 by tsishika          #+#    #+#             */
-/*   Updated: 2023/12/24 13:57:12 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/12/24 14:45:59 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 
 #define DEF_MESSAGE "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
+static void megaphone(const char *str){
+	for(int i = 0; str[i]; i++)
+		std::cout << (char)std::toupper(str[i]);
+}
+
 int main(int argc, char **argv){
 	if(argc == 1)
 		return (std::cout << DEF_MESSAGE << std::endl, 0);
-	for(int i = 1; i < argc; i++){
-		std::string str(argv[i]);
-		for(size_t j = 0; j < str.size(); j++)
-			std::cout << (char)std::toupper(str[j]);
-	}
+	for(int i = 1; i < argc; i++)
+		megaphone(argv[i]);
 	std::cout << std::endl;
 	return (0);
 }
